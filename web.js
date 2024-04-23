91ec5c0f6aecd9084978f0f1acd11c805f666eac
Sukhvinder Singh <147253173+sukhvindersingh730@users.noreply.github.com>
2024-04-23 13:13:00 +0100

    Add files via upload

---
+++ web.js
@@ -0,0 +1,21 @@
+let form_box=document.getElementById("form-box")
+let btn=document.getElementById("btn")
+btn.addEventListener('click',function(){
+    form_box.style.display='flex'
+  
+    
+})
+let cut = document.getElementById('cut-icon')
+cut.addEventListener('click',function(){
+    form_box.style.display='none';
+})
+
+
+    
+   
+    
+
+
+    
+
+
